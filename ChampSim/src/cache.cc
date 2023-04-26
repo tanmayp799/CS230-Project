7c6f15ccf6f5e801c41dee495c583abc95cd55a4
tanmayp799 <tanmayp799@gmail.com>
2023-04-26 09:39:48 +0530

    Minor correction cache.cc line#227

--- ChampSim/src/cache.cc
+++ ChampSim/src/cache.cc
@@ -224,7 +224,7 @@ void CACHE::handle_fill()
 
         uint8_t  do_fill = 1;
 
-        is this dirty?
+        // is this dirty?
         if (block[set][way].dirty) {
 
             // check if the lower level WQ has enough room to keep this writeback request
