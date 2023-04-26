815bfdef2d40ab6e5bc69e757c3f3630c30f8e40
harshitmorj <harshitmorj10@gmail.com>
2023-04-27 00:13:19 +0530

    Fixed some error

--- ChampSim/champsim.sh
+++ ChampSim/champsim.sh
@@ -88,8 +88,8 @@ cp cache_excl.cc ./src/cache.cc
 ./run_champsim.sh bimodal-no-no-no-no-lru-1core 30 30 cadical-high-60K-1227B.champsimtrace.xz &
 ./run_champsim.sh  bimodal-no-no-no-no-lru-1core 30 30 kissat-inc-high-30K-1802B.champsimtrace.xz &
 
-# ./build_champsim.sh bimodal no no no no lfu 1
-./run_champsim.sh bimodal-no-no-no-no-lfu-1core 30 30 cadical-high-60K-113B.champsimtrace.xz &
+./build_champsim.sh bimodal no no no no lfu 1
+# ./run_champsim.sh bimodal-no-no-no-no-lfu-1core 30 30 cadical-high-60K-113B.champsimtrace.xz &
 ./run_champsim.sh bimodal-no-no-no-no-lfu-1core 30 30 cadical-high-60K-134B.champsimtrace.xz &
 ./run_champsim.sh bimodal-no-no-no-no-lfu-1core 30 30 cadical-high-60K-1227B.champsimtrace.xz &
 ./run_champsim.sh  bimodal-no-no-no-no-lfu-1core 30 30 kissat-inc-high-30K-1802B.champsimtrace.xz &
